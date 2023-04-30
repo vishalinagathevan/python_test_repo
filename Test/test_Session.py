77ba5b13a87055f1e4d223827e9b762c30ee2aa8
vishalinagathevan <vishali.nagathevan@gmail.com>
2023-04-30 05:59:44 +0530

    changes in test_session script

--- Test/test_Session.py
+++ Test/test_Session.py
@@ -1,12 +1,15 @@
 from Development.Session import addition 
 
+
 # check add positive number
 def test_addPositive():
   assert addition(4,2) == 6
+
   
 # check if properly adds Zero 
 def test_addZero():
   assert addition(4,0) == 4
+
 
 # check with negative numbers
 def test_addNegative():
